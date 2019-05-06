9ad4dc1e622180a8198fdde9bc9c789dc12aa18b
Alex Li <alextianhongli@gmail.com>
2019-05-06 14:16:44 -0400

    center lists and fix loading screen

--- Manager/Controllers/StartDay.swift
+++ Manager/Controllers/StartDay.swift
@@ -62,12 +62,18 @@ class StartDay: UIViewController {
             }
         }
         let maxSize = CGSize(width: 150, height: 100)
-        task1.frame = CGRect(origin: CGPoint(x: 205, y: 225), size: task1.sizeThatFits(maxSize))
-        task2.frame = CGRect(origin: CGPoint(x: 205, y: 255), size: task2.sizeThatFits(maxSize))
-        task3.frame = CGRect(origin: CGPoint(x: 205, y: 285), size: task3.sizeThatFits(maxSize))
-        task4.frame = CGRect(origin: CGPoint(x: 205, y: 315), size: task4.sizeThatFits(maxSize))
-        task5.frame = CGRect(origin: CGPoint(x: 205, y: 345), size: task5.sizeThatFits(maxSize))
-        task6.frame = CGRect(origin: CGPoint(x: 205, y: 375), size: task6.sizeThatFits(maxSize))
+        task1.frame = CGRect(origin: CGPoint(x: 205-task1.sizeThatFits(maxSize).width/2, y: 225), size: task1.sizeThatFits(maxSize))
+        task2.frame = CGRect(origin: CGPoint(x: 205-task2.sizeThatFits(maxSize).width/2, y: 255), size: task2.sizeThatFits(maxSize))
+        task3.frame = CGRect(origin: CGPoint(x: 205-task3.sizeThatFits(maxSize).width/2, y: 285), size: task3.sizeThatFits(maxSize))
+        task4.frame = CGRect(origin: CGPoint(x: 205-task4.sizeThatFits(maxSize).width/2, y: 315), size: task4.sizeThatFits(maxSize))
+        task5.frame = CGRect(origin: CGPoint(x: 205-task5.sizeThatFits(maxSize).width/2, y: 345), size: task5.sizeThatFits(maxSize))
+        task6.frame = CGRect(origin: CGPoint(x: 205-task6.sizeThatFits(maxSize).width/2, y: 375), size: task6.sizeThatFits(maxSize))
+        task1.textAlignment = .center;
+        task2.textAlignment = .center;
+        task3.textAlignment = .center;
+        task4.textAlignment = .center;
+        task5.textAlignment = .center;
+        task6.textAlignment = .center;
     }
     
     @IBAction func dayStartStop(_ sender: Any) {
