938ba4fb4189be59aa34c91f5dfac4972831dcd9
Casey <caseycorvino7@gmail.com>
2019-05-07 20:46:59 -0400

    widget edited

--- extension/TodayViewController.swift
+++ extension/TodayViewController.swift
@@ -12,29 +12,36 @@ import NotificationCenter
 class TodayViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, NCWidgetProviding {
     let taskServices = TaskServices();
 
-
+    @IBOutlet weak var todoLabel: UILabel!
+    @IBOutlet weak var doingLabel: UILabel!
+    @IBOutlet weak var doneLabel: UILabel!
+    
     var tasks:[Task] = [];
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view from its nib.
         self.preferredContentSize = CGSize(width:self.view.frame.size.width, height:210)
-        
         if #available(iOSApplicationExtension 10.0, *) {
             self.extensionContext?.widgetLargestAvailableDisplayMode = .expanded
         }
         var calendar = NSCalendar.current
         calendar.timeZone = NSTimeZone.local;
         let startDay = calendar.startOfDay(for: Date());
+        var todoCount:Int = 0, startedCount:Int = 0, finishedCount:Int = 0;
         tasks = taskServices.LoadTasks(start: startDay, end: Date(timeInterval: 86400, since: startDay));
-        print(tasks.count);
-        var est:Int = 0;
-//        for t in tasks{
-//            if let e = Int(t.estimation){
-//                est += e;
-//            }
-//        }
-//        estimationLabel.text = "Estimated Time: " + String(est) + " hours";
+        for task in tasks {
+            if task.start == nil && task.end == nil{
+                todoCount += 1;
+            } else if task.start != nil && task.end == nil{
+                startedCount += 1
+            } else if task.start != nil && task.end != nil{
+                finishedCount += 1;
+            }
+        }
+        todoLabel.text = String(todoCount);
+        doingLabel.text = String(startedCount);
+        doneLabel.text = String(finishedCount);
     }
     
     @available(iOS 10.0, *)
